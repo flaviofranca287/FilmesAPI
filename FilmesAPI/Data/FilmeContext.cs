ad1ff137d195ab66cf5403945ed10ef1cd999212
Flavio França <flavio.santiago@stone.com.br>
2022-07-14 19:29:35 -0300

    Conectando ao banco de dados MySQL

---
+++ FilmesAPI/Data/FilmeContext.cs
@@ -0,0 +1,18 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeContext : DbContext //Mostro que ele será o conector entre o banco de dados com a API
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt) //Recebo as opções do filme context e o construtor da classe já recebe e faz tudo pra gente
+        {
+
+
+        }
+        // é o nosso conjunto de dados do banco que vamos conseguir fazzer de maneira encapsulada o acesso aos dados do banco.
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+
+}
--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,11 +1,18 @@
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+//Configura o serviço de acesso ao banco(adiciona contexto de banco de dados, o filme context, e as opções são simplesmente utilizar o MySQL, a string de conexão está definida em appsettings.json
+//A nossa string de conexão está definida no json(FilmeConnection)
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(builder.Configuration.GetConnectionString("FilmeConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
