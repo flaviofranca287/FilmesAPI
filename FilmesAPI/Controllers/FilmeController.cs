315aaa2cc278590669d91181014be1001332e516
Flavio França Stone <flavio.santiago@stone.com.br>
2022-07-14 11:46:54 -0300

    Validando parametros recebidos + HTTPGet

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -12,13 +12,19 @@ namespace FilmesAPI.Controllers
         private static List<Filme> filmes = new List<Filme>();
         //Para cadastrar o filme no sistema preciso ter uma função responsável por isso
 
+        private static int id = 1;
         [HttpPost]//Parametro para mostrar que iremos criar um recurso novo no sistema.
         //[FromBody] Eu preciso indicar que esse filme ta vindo através do corpo da minha requisição
         public void AdicionaFilme([FromBody]Filme filme)
         {
-            
+            filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+        }
+        [HttpGet]
+        //Poderia ser List<Filme> mas se em algum momento a gente não estiver retornando uma lista de filme, mas sim uma classe que implemente essa interface, com o IEnumerable, não vai quebrar.
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
         }
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,10 +1,19 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="O campo Título é obrigatório.")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage ="O campo Diretor é obrigatório.")]
         public string Diretor { get; set; }
+        [StringLength(20,ErrorMessage ="O campo Genero não pode passar de 20 caracteres.")]
         public string Genero { get; set; }
+        [Range(1,600,ErrorMessage ="A duração permitida é de 1 a 600.")]
         public int Duracao { get; set; }
+
     }
 }
